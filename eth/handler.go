c3d549122c1b62fd51193379b4db7ef647a087b8
pip.zhang <pip.zhang@cloudminds.com>
2020-01-13 16:38:14 +0800

    eth/downloader: avoid node data with zero length
    1.sending nodedata with zero length will cause the requesting peer retrieve
    this GetNodeDataMsg again and again, and can block block synchronise
    2.to fix this bug, we swallow this data. And as a result, it makes
    the requesting peer perform as timeout and then try others
    3.to make peer compatible with old peer sending zero node data, just
    avoid deliver this node data
    
    Depends-On: N/A

--- eth/handler.go
+++ eth/handler.go
@@ -601,6 +601,14 @@ func (pm *ProtocolManager) handleMsg(p *peer) error {
 				bytes += len(entry)
 			}
 		}
+		log.Debug("Sending node data",  "len", len(data))
+		if len(data) == 0 {
+			// in this case, sending node data with len=0 will cause the requesting peer retrieve this GetNodeDataMsg
+			// again and again, and can block block synchronise. So, we just swallow it, and as a result, it makes the
+			// requesting peer perform as timeout and then try others.
+			log.Warn("Sending node data is invalid, we swallow it",  "len", len(data))
+			return nil
+		}
 		return p.SendNodeData(data)
 
 	case p.version >= eth63 && msg.Code == NodeDataMsg:
@@ -608,6 +616,15 @@ func (pm *ProtocolManager) handleMsg(p *peer) error {
 		var data [][]byte
 		if err := msg.Decode(&data); err != nil {
 			return errResp(ErrDecode, "msg %v: %v", msg, err)
+		}
+		// to make peer compatible with old peer sending zero node data, just avoid deliver this node data.
+		// Why:
+		// deliver zero node data will cause statesync failing before other fetchers which interrupts all other
+		// fetchers, then all fetchers including statesync fetcher will run again and again until blocks from other
+		// peers were inserted
+		if len(data) == 0 {
+			log.Warn("No need to deliver zero len data")
+			return nil
 		}
 		// Deliver all to the downloader
 		if err := pm.downloader.DeliverNodeData(p.id, data); err != nil {
